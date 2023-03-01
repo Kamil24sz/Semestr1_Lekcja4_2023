3d2b0af57cd4e51f5ac92fc1726f47283bf76e80
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2023-03-01 18:16:48 +0100

    Add project files.

---
+++ Semestr1_Lekcja4_2023/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semestr1_Lekcja4_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 16Gb = 16 000Mb    1Gb = 1000Mb
+            // 1GB = 1024MB
+            Console.WriteLine("Podaj pojemność dysku deklarowaną przez producenta");
+            double pojemnoscProducenta = double.Parse(Console.ReadLine());
+            double pojemnoscFizyczna = pojemnoscProducenta * Math.Pow(1000,3);
+            double pojemnoscFaktyczna = pojemnoscFizyczna / Math.Pow(1024,3);
+            Console.WriteLine("Pojemność rzeczywista dysku to: " + Math.Round(pojemnoscFaktyczna, 2) + "GB");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Średnia_Ocen/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Średnia_Ocen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int matematyka = int.Parse(textBox1.Text);
+            int polski = int.Parse(textBox2.Text);
+            int fizyka = int.Parse(textBox3.Text);
+            int angielski = int.Parse(textBox4.Text);
+            int informatyka = int.Parse(textBox5.Text);
+
+            int suma = matematyka + polski + fizyka
+            + angielski + informatyka;
+
+            //rzotowanie - konwersja typów
+            float srednia = (float)suma / 5;
+
+            textBox6.Text = srednia.ToString();
+
+
+        }
+    }
+}
